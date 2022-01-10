d02ce778bc929dba6bb463be14aa5c2cd92541d0
Junior Rocco <juniorrocco2020@gmail.com>
2022-01-09 23:38:57 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoQuestaoNove/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoQuestaoNove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "ABC";
+
+            switch (a)
+            {
+                case "ABC":
+                case "A":
+                    {
+                        Console.WriteLine("A string tem ABC");
+                        break;
+                    }
+                case "BC":
+                    {
+                        Console.WriteLine("A string tem BC");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("String fora do padrão");
+                        break;
+                    }
+            }
+
+            
+        }
+    }
+}
